933e06b3d8763d38bd078f5e53640df88f54ff07
Julio Conchas <jconchas@ibm.com>
2025-04-06 18:55:10 -0600

    Adding extra files to config app in EB

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ from waitress import serve
 
 if __name__ == '__main__':
     #serve(app, host="0.0.0.0", port=5000) # Non-prod and prod
-    app.run(debug=True,port=8080) # test
+    app.run(debug=True,port=5000) # test
