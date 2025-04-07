db385ed437aaa866ac0ea5656d35961796cca61c
Julio Conchas <jconchas@ibm.com>
2025-04-07 15:40:12 -0600

    Adding appspect.yml file to deploy in EC2 Instancec

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from iconitodev import app
 from waitress import serve
 
 if __name__ == '__main__':
-    #serve(app, host="0.0.0.0", port=5000) # Non-prod and prod
-    app.run(debug=True,port=5000) # test
+    serve(app, host="0.0.0.0", port=5000) # Non-prod and prod
+    #app.run(debug=True,port=5000) # test
