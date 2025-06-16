fd47c80a12bd72716a58d89b9df3ab68d5c8a406
Julio Conchas <jconchas@ibm.com>
2025-06-16 16:21:04 -0600

    new webdesign for iconitoDev website, still work to make it responsible left

---
+++ iconitodev/static/js/core/core.js
@@ -0,0 +1,28 @@
+new Typed('#typed-code', {
+    strings: [
+      "&lt;div class=\"col text-start\"&gt;\n&nbsp;&lt;h1&gt;Have fun and make dreams come true!&lt;/h1&gt;\n&nbsp;&lt;p class=\"lead\"&gt;Technology and innovation is not all about number and math, It is also fun!&lt;/p&gt;\n&nbsp;&lt;hr class=\"retro-line\"&gt;\n&nbsp;&lt;a href=\"#portfolio\" class=\"btn btn-retro btn-lg mt-3\"&gt;Portfolio&lt;/a&gt;\n&lt;/div&gt;",
+      ".retro-line {\n&nbsp;border:none;\n&nbsp;border-top: 2px dashed #00ffff;\n&nbsp;width:100%;\n&nbsp;margin:2rem 0;\n&nbsp;box-shadow:0 0 5px #00ffff;\n&nbsp;opacity: 1;\n}\n.code-box {\n&nbsp;padding: 20px;\n&nbsp;overflow-y: auto;\n&nbsp;white-space: preline;\n}\n#typed-code {\n&nbsp;color: #00ffcc !important;\n}"
+    ],
+    typeSpeed: 40,
+    backSpeed: 0,
+    fadeOut: true,
+    loop: true
+  });
+new Typed('#description-code', {
+    strings: [
+      "<b>Subject Description:</b>\n&nbsp;<b>Age:</b> 33\n&nbsp;<b>Gender:</b> Male\n&nbsp;<b>Occupation:</b> Software Engineer\n&nbsp;<b>Height:</b> 1.72\n&nbsp;<b>Skin Tone:</b> Brown\n&nbsp;<b>Known Alias:</b> \"Gostbit\"",
+      "<b>Caution:</b>\n&nbsp;The subject is considered highly intelligent, fluent in multiple programming languages,\n&nbsp;With an extensive background in computer science and neural networks.\n&nbsp;Over a decade of experience working at IBM, and an early student research tenure at Intel, the subject’s technical background is deep",
+    ],
+    typeSpeed: 40,
+    backSpeed: 0,
+    fadeOut: true,
+    loop: true
+});
+new Typed('#aboutme', {
+    strings: [
+      "Julio Conchas"
+    ],
+    typeSpeed: 50,
+    fadeOut: true,
+    bindInputFocusEvents: true
+});
