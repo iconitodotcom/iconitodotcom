8cc67dc0d61a7728b8a104f558a58e9622c418f1
Julio Conchas <jconchas@ibm.com>
2025-07-05 12:36:17 -0600

    overlap hero cols for mobile version

--- iconitodev/static/js/core/core.js
+++ iconitodev/static/js/core/core.js
@@ -8,6 +8,16 @@ new Typed('#typed-code', {
     fadeOut: true,
     loop: true
   });
+new Typed('#typed-code-mobile', {
+    strings: [
+      "&lt;div class=\"col text-start\"&gt;\n&nbsp;&lt;h1&gt;Have fun and make dreams come true!&lt;/h1&gt;\n&nbsp;&lt;p class=\"lead\"&gt;Technology and innovation is not all about number and math, It is also fun!&lt;/p&gt;\n&nbsp;&lt;hr class=\"retro-line\"&gt;\n&nbsp;&lt;a href=\"#portfolio\" class=\"btn btn-retro btn-lg mt-3\"&gt;Portfolio&lt;/a&gt;\n&lt;/div&gt;",
+      ".retro-line {\n&nbsp;border:none;\n&nbsp;border-top: 2px dashed #00ffff;\n&nbsp;width:100%;\n&nbsp;margin:2rem 0;\n&nbsp;box-shadow:0 0 5px #00ffff;\n&nbsp;opacity: 1;\n}\n.code-box {\n&nbsp;padding: 20px;\n&nbsp;overflow-y: auto;\n&nbsp;white-space: preline;\n}\n#typed-code {\n&nbsp;color: #00ffcc !important;\n}"
+    ],
+    typeSpeed: 40,
+    backSpeed: 0,
+    fadeOut: true,
+    loop: true
+  });
 new Typed('#description-code', {
     strings: [
       "<b>Subject Description:</b>\n&nbsp;<b>Age:</b> 33\n&nbsp;<b>Gender:</b> Male\n&nbsp;<b>Occupation:</b> Software Engineer\n&nbsp;<b>Height:</b> 1.72\n&nbsp;<b>Skin Tone:</b> Brown\n&nbsp;<b>Known Alias:</b> \"Gostbit\"",
