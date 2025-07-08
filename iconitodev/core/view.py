c8a4eb3399a227d54f3a7bad1e5b1a3d08fe7c36
Julio Conchas <jconchas@ibm.com>
2025-07-08 12:20:31 -0600

    #6, #8 Login view and adding more space to contact section for mobile version

--- iconitodev/core/view.py
+++ iconitodev/core/view.py
@@ -1,4 +1,7 @@
 #iconitodev/core/view.py
+#issue dev           date       description
+# 6    Julio Conchas 07/08/2025 Adding login view
+
 from flask import render_template,request,Blueprint
 
 core = Blueprint('core',__name__)
@@ -6,3 +9,7 @@ core = Blueprint('core',__name__)
 @core.route('/')
 def index():
     return render_template('index.html')
+
+@core.route('/login')
+def login():
+    return render_template('login.html')
